6cdaf33fa51239a0bfa67495cb418a42f483ff63
Owen Sowatzke <osowatzke@gmail.com>
2021-04-06 15:03:36 -0400

    Created function to create complex noise with unit variance

---
+++ NoiseGeneration.m
@@ -0,0 +1,14 @@
+% function generates complex white Gaussion noise with zero mean and 
+% unit variance
+function Noise = NoiseGeneration(Length)
+    
+    % generate real noise of with variance 1/sqrt(2)
+    real_noise = 1/sqrt(2)*randn(1,Length);
+    
+    % generate complex noise with variance 1/sqrt(2)
+    imag_noise = 1/sqrt(2)*randn(1,Length);
+    
+    % sum both real and imaginary noise to get complex noise
+    Noise = real_noise + imag_noise;
+    
+end
